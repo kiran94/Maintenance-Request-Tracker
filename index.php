f4fbbb59b11822fb28071086bd3938039f16421c
Kiran Patel <kiranpatel@Kirans-MacBook-Pro.local>
2014-09-19 13:31:50 +0100

    Created a template to which content and the form will be added

--- index.php
+++ index.php
@@ -1,10 +1,44 @@
 <!DOCTYPE html> 
 <html>
 <head>
-	<title>Maintence Request Tracker</title>
+	<title>Maintenance Request Tracker</title>
+	<link rel="stylesheet" type="text/css" href="style/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="style/style.css" />
 </head>
 <body>
-	
 
+	<!-- CONTAINER -->
+	<div class="container">
+		<h1 id='title'>Maintenance Requester</h1>
+
+
+		<!-- CONTENT -->
+		<div class='body-content'>
+			dfodfg;dfg
+
+		</div>
+		<!-- END CONTENT -->
+
+
+		<!-- FOOTER -->
+		<div id='body-footer'>
+			Developed By Kiran Patel<br/>
+			View the <a href='https://github.com/kiran94/Maintenance-Request-Tracker'>project!</a>  
+		</div>
+
+		<!-- END FOOTER -->
+
+
+
+
+
+
+	<!-- END CONTAINER -->
+	</div>
+
+
+
+	<!-- Need jquery -->
+	<script type="text/javascript" src="script/bootstrap.min.js"></script>
 </body>
 </html>
